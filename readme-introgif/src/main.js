671845196c47a202e930253be9a29bc7afa7b25a
Sushant mall <52692251+sushant-mall@users.noreply.github.com>
2020-10-11 11:00:26 +0530

    Initial Commit

---
+++ readme-introgif/src/main.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import App from "./App.vue";
+import TypeIt from "typeit";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
+
+new TypeIt("#inner-demo-2", {
+  speed: 100,
+  lifelike: true,
+  cursor: true,
+  cursorSpeed: 300,
+  loop: true
+})
+  .pause(1000)
+  .type('<span style="font-family: Segoe UI Emoji">👋</span>', {
+    html: true
+  })
+  .type("&nbspHi&nbspthere!&nbspI'm&nbspSusha111")
+  .pause(750)
+  .delete(3, { deleteSpeed: 130 })
+  .pause(500)
+  .type("nt&nbspMall")
+  .pause(500)
+  .move("END")
+  .go();
